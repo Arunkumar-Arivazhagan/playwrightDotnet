069ccd56e328c2326ea01c6b571b04f14ae521d6
Arunkumar <arunkumar041093@gmail.com>
2024-03-11 13:22:50 +1300

    Playwright config

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,15 +1,36 @@
-namespace playwright;
+// using Microsoft.Playwright;
 
-public class Tests
-{
-    [SetUp]
-    public void Setup()
-    {
-    }
+// namespace playwright;
 
-    [Test]
-    public void Test1()
-    {
-        Assert.Pass();
-    }
-}
+// public class Tests
+// {
+//     [SetUp]
+//     public void Setup()
+//     {
+//     }
+
+//     [Test]
+//     public async Task Test1()
+//     {
+//         using var playwright = await Playwright.CreateAsync();
+
+//         //browser
+//         await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+//         {
+//             Headless = false
+//         });
+
+//         //page
+//         var page = await browser.NewPageAsync();
+//         await page.GotoAsync(url: "http//www.eaapp.somee.com");
+//         await page.ClickAsync(selector: "text=Login");
+//         await page.ScreenshotAsync(new PageScreenshotOptions
+//         {
+//             Path = "EAApp.jpg"
+//         });
+//         await page.FillAsync(selector: "#UserName", value: "admin");
+//         await page.FillAsync(selector: "#Password", value: "password");
+//         await page.ClickAsync(selector: "text=Log in");
+//         var isExist = await page.Locator(selector: "text='Employee Details'").IsVisibleAsync();
+//     }
+// }
